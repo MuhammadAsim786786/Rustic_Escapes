d1940b70b79555b0ad6bfc92fc855fc95661520b
MuhammadAsim786786 <asim.leo3243@gmail.com>
2025-05-24 00:14:28 +0500

    Final Filtering,sorting and pagination

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -10,6 +10,11 @@ function Account() {
         <Heading as="h3">Update user data</Heading>
         <p>Update user data form</p>
       </Row>
+
+      <Row>
+        <Heading as="h3">Update password</Heading>
+        <p>Update user password form</p>
+      </Row>
     </>
   );
 }
