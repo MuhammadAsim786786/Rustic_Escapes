a56fe996c2b558ef376c33276b34cbfc2fe665c9
Rizwanhu <rizwanhussain66@hotmail.com>
2025-05-25 12:48:21 +0500

    Resolving Merge Conflicts

--- src/features/bookings/BookingDataBox.jsx
+++ src/features/bookings/BookingDataBox.jsx
@@ -1,28 +1,10 @@
-<<<<<<< HEAD
-import styled from 'styled-components';
-import { format } from 'date-fns';
-
-import { box } from 'styles/styles';
-import { formatDistanceFromNow } from 'utils/helpers';
-import { isToday } from 'date-fns/esm';
-import { formatCurrency } from 'utils/helpers';
-=======
 import styled from "styled-components";
 import { format, isToday } from "date-fns";
->>>>>>> origin/final-4
 import {
   HiOutlineChatBubbleBottomCenterText,
   HiOutlineCheckCircle,
   HiOutlineCurrencyDollar,
   HiOutlineHomeModern,
-<<<<<<< HEAD
-} from 'react-icons/hi2';
-import DataItem from 'ui/DataItem';
-import { Flag } from 'ui/Flag';
-
-const StyledBookingDataBox = styled.section`
-  ${box} /* padding: 3.2rem 4rem; */
-=======
 } from "react-icons/hi2";
 
 import DataItem from "../../ui/DataItem";
@@ -36,16 +18,11 @@ const StyledBookingDataBox = styled.section`
   border: 1px solid var(--color-grey-100);
   border-radius: var(--border-radius-md);
 
->>>>>>> origin/final-4
   overflow: hidden;
 `;
 
 const Header = styled.header`
   background-color: var(--color-brand-500);
-<<<<<<< HEAD
-  /* padding: 2.4rem 4rem; */
-=======
->>>>>>> origin/final-4
   padding: 2rem 4rem;
   color: #e0e7ff;
   font-size: 1.8rem;
@@ -68,11 +45,7 @@ const Header = styled.header`
   }
 
   & span {
-<<<<<<< HEAD
-    font-family: 'Sono';
-=======
     font-family: "Sono";
->>>>>>> origin/final-4
     font-size: 2rem;
     margin-left: 4px;
   }
@@ -86,10 +59,6 @@ const Guest = styled.div`
   display: flex;
   align-items: center;
   gap: 1.2rem;
-<<<<<<< HEAD
-  /* font-size: 1.8rem; */
-=======
->>>>>>> origin/final-4
   margin-bottom: 1.6rem;
   color: var(--color-grey-500);
 
@@ -108,15 +77,9 @@ const Price = styled.div`
   margin-top: 2.4rem;
 
   background-color: ${(props) =>
-<<<<<<< HEAD
-    props.isPaid ? 'var(--color-green-100)' : 'var(--color-yellow-100)'};
-  color: ${(props) =>
-    props.isPaid ? 'var(--color-green-700)' : 'var(--color-yellow-700)'};
-=======
     props.isPaid ? "var(--color-green-100)" : "var(--color-yellow-100)"};
   color: ${(props) =>
     props.isPaid ? "var(--color-green-700)" : "var(--color-yellow-700)"};
->>>>>>> origin/final-4
 
   & p:last-child {
     text-transform: uppercase;
@@ -138,10 +101,7 @@ const Footer = styled.footer`
   text-align: right;
 `;
 
-<<<<<<< HEAD
-=======
 // A purely presentational component
->>>>>>> origin/final-4
 function BookingDataBox({ booking }) {
   const {
     created_at,
@@ -170,19 +130,11 @@ function BookingDataBox({ booking }) {
         </div>
 
         <p>
-<<<<<<< HEAD
-          {format(new Date(startDate), 'EEE, MMM dd yyyy')} (
-          {isToday(new Date(startDate))
-            ? 'Today'
-            : formatDistanceFromNow(startDate)}
-          ) &mdash; {format(new Date(endDate), 'EEE, MMM dd yyyy')}
-=======
           {format(new Date(startDate), "EEE, MMM dd yyyy")} (
           {isToday(new Date(startDate))
             ? "Today"
             : formatDistanceFromNow(startDate)}
           ) &mdash; {format(new Date(endDate), "EEE, MMM dd yyyy")}
->>>>>>> origin/final-4
         </p>
       </Header>
 
@@ -190,11 +142,7 @@ function BookingDataBox({ booking }) {
         <Guest>
           {countryFlag && <Flag src={countryFlag} alt={`Flag of ${country}`} />}
           <p>
-<<<<<<< HEAD
-            {guestName} {numGuests > 1 ? `+ ${numGuests - 1} guests` : ''}
-=======
             {guestName} {numGuests > 1 ? `+ ${numGuests - 1} guests` : ""}
->>>>>>> origin/final-4
           </p>
           <span>&bull;</span>
           <p>{email}</p>
@@ -205,23 +153,14 @@ function BookingDataBox({ booking }) {
         {observations && (
           <DataItem
             icon={<HiOutlineChatBubbleBottomCenterText />}
-<<<<<<< HEAD
-            label='Observations'
-=======
             label="Observations"
->>>>>>> origin/final-4
           >
             {observations}
           </DataItem>
         )}
 
-<<<<<<< HEAD
-        <DataItem icon={<HiOutlineCheckCircle />} label='Breakfast included?'>
-          {hasBreakfast ? 'Yes' : 'No'}
-=======
         <DataItem icon={<HiOutlineCheckCircle />} label="Breakfast included?">
           {hasBreakfast ? "Yes" : "No"}
->>>>>>> origin/final-4
         </DataItem>
 
         <Price isPaid={isPaid}>
@@ -234,20 +173,12 @@ function BookingDataBox({ booking }) {
               )} breakfast)`}
           </DataItem>
 
-<<<<<<< HEAD
-          <p>{isPaid ? 'Paid' : 'Will pay at property'}</p>
-=======
           <p>{isPaid ? "Paid" : "Will pay at property"}</p>
->>>>>>> origin/final-4
         </Price>
       </Section>
 
       <Footer>
-<<<<<<< HEAD
-        <p>Booked {format(new Date(created_at), 'EEE, MMM dd yyyy, p')}</p>
-=======
         <p>Booked {format(new Date(created_at), "EEE, MMM dd yyyy, p")}</p>
->>>>>>> origin/final-4
       </Footer>
     </StyledBookingDataBox>
   );
--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
-import {
-  HiPencil,
-  HiTrash,
-  HiEye,
-  HiArrowUpOnSquare,
-  HiArrowDownOnSquare,
-} from 'react-icons/hi2';
-
-import Tag from 'ui/Tag';
-import Menus from 'ui/Menus';
-import Modal from 'ui/Modal';
-import ConfirmDelete from 'ui/ConfirmDelete';
-import Table from 'ui/Table';
-
-import { useDeleteBooking } from 'features/bookings/useDeleteBooking';
-import { formatCurrency } from 'utils/helpers';
-import { formatDistanceFromNow } from 'utils/helpers';
-import { useCheckout } from 'features/check-in-out/useCheckout';
-import { format, isToday } from 'date-fns';
-
-// v1
-// const TableRow = styled.div`
-//   display: grid;
-//   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-//   column-gap: 2.4rem;
-//   align-items: center;
-//   padding: 1.4rem 2.4rem;
-
-//   &:not(:last-child) {
-//     border-bottom: 1px solid var(--color-grey-100);
-//   }
-// `;
-=======
 import styled from "styled-components";
 import { format, isToday } from "date-fns";
 import {
@@ -54,17 +18,12 @@ import { formatCurrency } from "../../utils/helpers";
 import { formatDistanceFromNow } from "../../utils/helpers";
 import { useCheckout } from "../check-in-out/useCheckout";
 import { useDeleteBooking } from "./useDeleteBooking";
->>>>>>> origin/final-4
 
 const Cabin = styled.div`
   font-size: 1.6rem;
   font-weight: 600;
   color: var(--color-grey-600);
-<<<<<<< HEAD
-  font-family: 'Sono';
-=======
   font-family: "Sono";
->>>>>>> origin/final-4
 `;
 
 const Stacked = styled.div`
@@ -83,11 +42,7 @@ const Stacked = styled.div`
 `;
 
 const Amount = styled.div`
-<<<<<<< HEAD
-  font-family: 'Sono';
-=======
   font-family: "Sono";
->>>>>>> origin/final-4
   font-weight: 500;
 `;
 
@@ -105,23 +60,6 @@ function BookingRow({
     cabins: { name: cabinName },
   },
 }) {
-<<<<<<< HEAD
-  const { mutate: deleteBooking, isLoading: isDeleting } = useDeleteBooking();
-  const { mutate: checkout, isLoading: isCheckingOut } = useCheckout();
-
-  const navigate = useNavigate();
-
-  // We will not allow editing at this point, as it's too complex for bookings... People just need to delete a booking and create a new one
-
-  const statusToTagName = {
-    unconfirmed: 'blue',
-    'checked-in': 'green',
-    'checked-out': 'silver',
-  };
-
-  return (
-    <Table.Row role='row'>
-=======
   const navigate = useNavigate();
   const { checkout, isCheckingOut } = useCheckout();
   const { deleteBooking, isDeleting } = useDeleteBooking();
@@ -134,7 +72,6 @@ function BookingRow({
 
   return (
     <Table.Row>
->>>>>>> origin/final-4
       <Cabin>{cabinName}</Cabin>
 
       <Stacked>
@@ -145,23 +82,6 @@ function BookingRow({
       <Stacked>
         <span>
           {isToday(new Date(startDate))
-<<<<<<< HEAD
-            ? 'Today'
-            : formatDistanceFromNow(startDate)}{' '}
-          &rarr; {numNights} night stay
-        </span>
-        <span>
-          {format(new Date(startDate), 'MMM dd yyyy')} &mdash;{' '}
-          {format(new Date(endDate), 'MMM dd yyyy')}
-        </span>
-      </Stacked>
-
-      <Tag type={statusToTagName[status]}>{status.replace('-', ' ')}</Tag>
-
-      <Amount>{formatCurrency(totalPrice)}</Amount>
-
-      {/* VIDEO we could export this into own component... */}
-=======
             ? "Today"
             : formatDistanceFromNow(startDate)}{" "}
           &rarr; {numNights} night stay
@@ -176,93 +96,36 @@ function BookingRow({
 
       <Amount>{formatCurrency(totalPrice)}</Amount>
 
->>>>>>> origin/final-4
       <Modal>
         <Menus.Menu>
           <Menus.Toggle id={bookingId} />
           <Menus.List id={bookingId}>
             <Menus.Button
-<<<<<<< HEAD
-              onClick={() => navigate(`/bookings/${bookingId}`)}
-              icon={<HiEye />}
-=======
               icon={<HiEye />}
               onClick={() => navigate(`/bookings/${bookingId}`)}
->>>>>>> origin/final-4
             >
               See details
             </Menus.Button>
 
-<<<<<<< HEAD
-            {status === 'unconfirmed' && (
-              <Menus.Button
-                onClick={() => navigate(`/checkin/${bookingId}`)}
-                icon={<HiArrowDownOnSquare />}
-=======
             {status === "unconfirmed" && (
               <Menus.Button
                 icon={<HiArrowDownOnSquare />}
                 onClick={() => navigate(`/checkin/${bookingId}`)}
->>>>>>> origin/final-4
               >
                 Check in
               </Menus.Button>
             )}
 
-<<<<<<< HEAD
-            {status === 'checked-in' && (
-              <Menus.Button
-                onClick={() => checkout(bookingId)}
-                disabled={isCheckingOut}
-                icon={<HiArrowUpOnSquare />}
-=======
             {status === "checked-in" && (
               <Menus.Button
                 icon={<HiArrowUpOnSquare />}
                 onClick={() => checkout(bookingId)}
                 disabled={isCheckingOut}
->>>>>>> origin/final-4
               >
                 Check out
               </Menus.Button>
             )}
 
-<<<<<<< HEAD
-            <Menus.Button icon={<HiPencil />}>Edit booking</Menus.Button>
-            {/* <Menus.Button>Delete</Menus.Button> */}
-
-            {/* Now it gets a bit confusing... */}
-            <Modal.Toggle opens='delete'>
-              <Menus.Button icon={<HiTrash />}>Delete booking</Menus.Button>
-            </Modal.Toggle>
-          </Menus.List>
-        </Menus.Menu>
-
-        {/* This needs to be OUTSIDE of the menu, which in no problem. The compound component gives us this flexibility */}
-        <Modal.Window name='delete'>
-          <ConfirmDelete
-            resource='booking'
-            // These options will be passed wherever the function gets called, and they determine what happens next
-            onConfirm={(options) => deleteBooking(bookingId, options)}
-            disabled={isDeleting}
-          />
-        </Modal.Window>
-      </Modal>
-
-      {/* <div>
-        <ButtonWithConfirm
-          title='Delete booking'
-          description='Are you sure you want to delete this booking? This action can NOT be undone.'
-          confirmBtnLabel='Delete'
-          onConfirm={() => deleteBooking(bookingId)}
-          disabled={isDeleting}
-        >
-          Delete
-        </ButtonWithConfirm>
-
-        <Link to={`/bookings/${bookingId}`}>Details &rarr;</Link>
-      </div> */}
-=======
             <Modal.Open opens="delete">
               <Menus.Button icon={<HiTrash />}>Delete booking</Menus.Button>
             </Modal.Open>
@@ -277,7 +140,6 @@ function BookingRow({
           />
         </Modal.Window>
       </Modal>
->>>>>>> origin/final-4
     </Table.Row>
   );
 }
